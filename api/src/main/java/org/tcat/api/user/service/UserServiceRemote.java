47d815ce74eee4bfbcd13c2553469bb4ed38de8c
lin_mr-world <1300643272@qq.com>
2018-03-05 10:55:13 +0800

    commit

--- api/src/main/java/org/tcat/api/user/service/UserServiceRemote.java
+++ api/src/main/java/org/tcat/api/user/service/UserServiceRemote.java
@@ -11,6 +11,9 @@ import org.tcat.api.user.dto.UserDTO;
 @FeignClient(name = "spring_cloud_model")
 public interface UserServiceRemote {
 
+    /**
+     * 暂时找不到，每个接口类都要写path的方法。如果有好的方法或思路，欢迎在issues中提出来
+     */
     String PATH = "user-service-remote/";
 
     @PostMapping(PATH + "test1")
