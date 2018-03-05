28042b4efeb5bc80513466905c898255d40d5a92
lin_mr-world <1300643272@qq.com>
2018-03-05 10:45:09 +0800

    commit

--- service/src/main/java/org/tcat/service/Application.java
+++ service/src/main/java/org/tcat/service/Application.java
@@ -2,9 +2,6 @@ package org.tcat.service;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * 用户服务 启动器
